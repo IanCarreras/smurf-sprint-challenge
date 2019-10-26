b29bdeb19c85001ac2eb8dab68e10df85ecf4e7e
Ian <mus5002@aol.com>
2019-10-26 12:33:21 -0400

    setup redux, created fetch smurfs action and reducer

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,36 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { actionCreators } from '../actions/actions'
+
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
+
+const App = props => {
+  const { smurfs, actions } = props
+
+  useEffect(() => {
+    actions.getSmurfs()
+  }, [actions])
+
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <div>Start inside of your `src/index.js` file!</div>
+      <div>Have fun!</div>
+    </div>
+  );
+}
+
+
+const mapStateToProps = ({ smurfs }) => {
+  return { smurfs }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    actions: bindActionCreators(actionCreators, dispatch)
   }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ smurfs/src/reducers/reducers.js
@@ -0,0 +1,36 @@
+import {
+    FETCH_SMURFS_START,
+    FETCH_SMURFS_ERROR,
+    FETCH_SMURFS_SUCCESS
+} from '../actions/actions'
+
+const initialState = {
+    smurfs: 'smurf'
+}
+
+const reducer = (state = initialState, action) => {
+    const { type, payload } = action 
+    switch (type) {
+        case FETCH_SMURFS_START:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case FETCH_SMURFS_ERROR:
+            return {
+                ...state,
+                error: payload,
+                isLoading: false
+            }
+        case FETCH_SMURFS_SUCCESS:
+            return {
+                ...state,
+                smurfs: payload,
+                isLoading: false
+            }
+        default:
+            return state
+    }
+}
+
+export default reducer
